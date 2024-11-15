1744e1036e2c7f32d633ea5a4e937c23457b0476
uzumaki0312 <mateusblindao2001@gmail.com>
2024-11-14 22:59:25 -0300

    Add files via upload

---
+++ admin/version.php
@@ -0,0 +1,8 @@
+<h2>
+        <a href="https://yellowweb.top/donate" target="_blank">
+                Version: 15.08.2024
+                <br />
+                PHP: <?= PHP_VERSION ?>
+                <br /> Please, donate!
+        </a>
+</h2>
